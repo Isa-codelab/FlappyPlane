1d38487dcea48e782048e521002bf34eb5d930f3
Isabelle <isasoarez21@gmail.com>
2025-08-15 20:45:45 -0300

    feat: add player moviment

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //movimento vertical do player
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * moveSpeed;
+        }
+
+        //limita a velocidade 
+        if (rb.velocity.y < -moveSpeed)
+        {
+            rb.velocity = Vector2.down * moveSpeed;
+        }
     }
 }
