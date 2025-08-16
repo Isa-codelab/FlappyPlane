151fb3b965cca6ebac102555cef9c6d61e287329
Isabelle <isasoarez21@gmail.com>
2025-08-15 23:28:36 -0300

    feat: define time to destroy the prefab

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class ObstacleController : MonoBehaviour
 {   
     [SerializeField ]private float velocity = 5f;
+    [SerializeField] private GameObject me;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(me, 5f);
     }
 
     // Update is called once per frame
