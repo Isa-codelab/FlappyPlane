f12fcfa10039987a09c38401cab0ad66917ec198
Isabelle <isasoarez21@gmail.com>
2025-08-16 11:05:11 -0300

    feat: add scores

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    private float score = 0f;
     [SerializeField] private float spawntMax = 3f;
     [SerializeField] private float spawntMin = 1f;
 
@@ -12,8 +14,10 @@ public class GameController : MonoBehaviour
     [SerializeField] private GameObject obstacle;
     [SerializeField] private Vector3 posicao;
 
-    [SerializeField] private float posMin = -0.3f;
-    [SerializeField] private float posMax = 2.4f;
+    [SerializeField] private float posMin = -0.5f;
+    [SerializeField] private float posMax = 3f;
+
+    [SerializeField] private TextMeshProUGUI scoreText;
     void Start()
     {
         
@@ -21,15 +25,31 @@ public class GameController : MonoBehaviour
 
     void Update()
     {
+        Scores();
+
+        SpawnObstacle();
+
+    }
+
+    private void Scores()
+    {
+        
+        score += Time.deltaTime;
+
+        scoreText.text = "Score: " + Mathf.RoundToInt(score).ToString();
+
+    }
+
+    private void SpawnObstacle()
+    {
         timer -= Time.deltaTime;
         if (timer <= 0f)
         {
             timer = Random.Range(spawntMin, spawntMax);
 
-            posicao.y = Random.Range(posMin,posMax);
+            posicao.y = Random.Range(posMin, posMax);
 
-            Instantiate(obstacle,posicao, Quaternion.identity);
+            Instantiate(obstacle, posicao, Quaternion.identity);
         }
-        
     }
 }
