99a58fa55510dc07a7213c179d34b7507db70666
Isabelle <isasoarez21@gmail.com>
2025-08-15 23:36:57 -0300

    feat: set a interval to spawn the obstacles

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField] private float spawntMax = 3f;
+    [SerializeField] private float spawntMin = 1f;
+
     [SerializeField] private float timer = 1f;
+
     [SerializeField] private GameObject obstacle;
     [SerializeField] private Vector3 posicao;
+
     [SerializeField] private float posMin = -0.3f;
     [SerializeField] private float posMax = 2.4f;
     void Start()
@@ -19,7 +24,7 @@ public class GameController : MonoBehaviour
         timer -= Time.deltaTime;
         if (timer <= 0f)
         {
-            timer = 1f;
+            timer = Random.Range(spawntMin, spawntMax);
 
             posicao.y = Random.Range(posMin,posMax);
 
